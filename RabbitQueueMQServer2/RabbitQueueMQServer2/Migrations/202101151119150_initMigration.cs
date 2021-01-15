2ab3a564e897a33f1611f57f6efb4026c67b3c85
skyfury2651 <skyfury2651@gmail.com>
2021-01-15 18:50:27 +0700

    first commit

---
+++ RabbitQueueMQServer2/RabbitQueueMQServer2/Migrations/202101151119150_initMigration.cs
@@ -0,0 +1,30 @@
+﻿namespace RabbitQueueMQServer2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initMigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.News",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UrlBasePost = c.String(),
+                        Title = c.String(),
+                        UrlImg = c.String(),
+                        Content = c.String(),
+                        Description = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.News");
+        }
+    }
+}
---
+++ RabbitQueueMQServer2/RabbitQueueMQServer2/News.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitQueueMQServer2
+{
+    class News
+    {
+        public int Id { get; set; }
+        public string UrlBasePost { get; set; }
+        public string Title { get; set; }
+        public string UrlImg { get; set; }
+        public string Content { get; set; }
+        public string Description { get; set; }
+    }
+}
